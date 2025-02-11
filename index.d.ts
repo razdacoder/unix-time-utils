0c0789c507491c4ad7df9e897d04637144d5350e
Ismail AbdulAzeez <ismailabdulazeez100@gmail.com>
2025-02-11 09:59:15 +0100

    added types

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export declare function unixTimeInSeconds(seconds: number): number;
+export declare function unixTimeInMinutes(minutes: number): number;
+export declare function unixTimeInHours(hours: number): number;
+export declare function unixTimeInDays(days: number): number;
